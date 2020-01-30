8ed03d81f31c793e6e30389e47a73c35909057fe
Rishav <kumarrishav011@gmail.com>
2020-01-31 00:26:32 +0530

    chat Activity

---
+++ app/src/main/java/com/example/fireapp/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.example.fireapp.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.fireapp.R;
+import com.example.fireapp.tab1;
+import com.example.fireapp.tab2;
+import com.example.fireapp.tab3;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2,R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+       switch (position){
+           case 0 :
+               tab1 tab1 = new tab1();
+               return tab1;
+           case 1:
+               tab2 tab2 = new tab2();
+               return tab2;
+           case  2:
+               tab3 tab3 = new tab3();
+               return tab3;
+
+       }
+       return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+}
